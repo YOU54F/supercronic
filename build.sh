878cb4dfed104626a25116c4cd0ffc41301481ce
Yousaf Nabi <yousafn@gmail.com>
2024-03-21 02:40:56 +0000

    add more targets

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -x
 mkdir -p dist
 export GOOS="linux"
 export CGO_ENABLED=0
-for arch in amd64 386 arm arm64; do GOARCH="$arch" go build && file supercronic | grep 'statically linked' && mv supercronic "dist/supercronic-${GOOS}-${arch}"; done
+for arch in amd64 386 amd64p32 arm arm64 mips mips64 mipsle mips64le ppc64 ppc64le riscv riscv64 s390 s390x; do GOARCH="$arch" go build && file supercronic | grep 'statically linked' && mv supercronic "dist/supercronic-${GOOS}-${arch}"; done
 pushd dist
 ls -lah *
 file *
